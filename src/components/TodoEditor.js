1e4aa41336926db3c49fb72d6134bbacf85700ca
RangCloud <mtr008@naver.com>
2023-11-06 11:27:19 +0900

    231106

---
+++ src/components/TodoEditor.js
@@ -0,0 +1,15 @@
+import "./TodoEditor.css";
+
+const TodoEditor = ()=>{
+    return(
+        <div className="TodoEditor">
+            <h3>새로운 Todo 작성하기 ✏</h3>
+            <div>
+                <input/>
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default TodoEditor;
---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import "./TodoList.css";
+
+const TodoList = ()=>{
+    return(
+        <div className="TodoList">
+            <h3>Todo List 🌱</h3>
+        </div>
+    )
+}
+
+export default TodoList;

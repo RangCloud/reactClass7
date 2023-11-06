b1e7f40db13e3dfb345d284311f63c777f2b61be
RangCloud <mtr008@naver.com>
2023-11-06 12:40:31 +0900

    231106

--- src/components/TodoEditor.js
+++ src/components/TodoEditor.js
@@ -1,12 +1,34 @@
 import "./TodoEditor.css";
+import {useState} from "react";
 
-const TodoEditor = ()=>{
+const TodoEditor = ({onCreate})=>{
+    const [content, setContent] = useState("");
+
+    const onChangeTodo = (event)=>{
+        setContent(event.target.value);
+    }
+
+    const onSubmit = ()=>{
+        if(!content){return;}
+        onCreate(content);
+        setContent("");
+    }
+
+    const onKeyDownHandler = (event)=>{
+        if(event.keyCode === 13){ // keyCode 값 엔터
+            onSubmit();
+        }
+    }
+
     return(
         <div className="TodoEditor">
             <h3>새로운 Todo 작성하기 ✏</h3>
             <div>
-                <input/>
-                <button></button>
+                <input 
+                onChange={onChangeTodo}
+                onKeyDown={onKeyDownHandler}
+                placeholder="새로운 Todo..."/>
+                <button onClick={onSubmit}>추가</button>
             </div>
         </div>
     )
